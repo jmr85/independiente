9e8931f5ad46bbc8dac5aaed34b3d2e3b3b9eeca
Juan Martin Ruiz <juan.martin.ruiz@gmail.com>
2017-01-30 06:27:35 -0300

    probando navegacion 2 ajax

--- js/navegacion.js
+++ js/navegacion.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     $('#club').click(function () {
         // $('#contenido-principal').html('js/formulario.html #clase-row');//NO anda
         $.ajax({
-            url: "/js/formulario.html",
+            url: "js/formulario.html",
             cache: true,
             success: function (contenido) {
                 $('#contenido-principal').html(contenido);
