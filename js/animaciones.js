8821b8163051176439b2f282b41898fe8a3055ff
Juan Martin Ruiz <juan.martin.ruiz@gmail.com>
2017-01-30 06:19:03 -0300

    probando navegacion mediante ajax

---
+++ js/animaciones.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+    
+   /* $('#club').click(function(){//que tenga animacion el logo
+        $(this).animate({
+            left: 100,
+            top: 100,
+            heigth: 200,
+            width: 600
+        }, 1000);
+    });*/
+
+
+});
---
+++ js/navegacion.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+    $('#club').click(function () {
+        // $('#contenido-principal').html('js/formulario.html #clase-row');//NO anda
+        $.ajax({
+            url: "/js/formulario.html",
+            cache: true,
+            success: function (contenido) {
+                $('#contenido-principal').html(contenido);
+            }
+        });
+    });
+});
+
+        
+ 
+
+
+
